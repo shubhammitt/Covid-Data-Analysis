e3026554286ae107fcfe42b9d0e3bdb57ec42469
Shubham Mittal <shubham18101@iiitd.ac.in>
2020-09-11 20:02:01 +0530

    Initial Setup to start Assignment

---
+++ Assn1.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+    """
+    Import Statements
+    """
+import pandas as pd
+#... rest of the imports
+
+def Q1_1(json_file_path, start_date, end_date):
+    """Q1 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    print('confirmed_count: ',confirmed_count, 'recovered_count: ',recovered_count, 'deceased_count: ',deceased_count)
+    return confirmed_count, recovered_count, deceased_count
+
+def Q1_2(json_file_path, start_date, end_date):
+    """Q1 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    print('confirmed_count: ',confirmed_count, 'recovered_count: ',recovered_count, 'deceased_count: ',deceased_count)
+    return confirmed_count, recovered_count, deceased_count
+
+def Q1_3(json_file_path, start_date, end_date):
+    """Q1 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    print('confirmed_count: ',confirmed_count, 'recovered_count: ',recovered_count, 'deceased_count: ',deceased_count)
+    return confirmed_count, recovered_count, deceased_count
+
+def Q1_4(json_file_path, start_date, end_date):
+    """Q1 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    print('Confirmed \n')
+    print('Highest affected State is: ')
+    print('Highest affected State count is: ')
+    print('Recovered \n')
+    print('Highest affected State is: ')
+    print('Highest affected State count is: ')
+    print('Deceased \n')
+    print('Highest affected State is: ')
+    print('Highest affected State count is: ')
+
+
+def Q1_5(json_file_path, start_date, end_date):
+    """Q1 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    print('Confirmed \n')
+    print('Lowest affected State is: ')
+    print('Lowest affected State count is: ')
+    print('Recovered \n')
+    print('Lowest affected State is: ')
+    print('Lowest affected State count is: ')
+    print('Deceased \n')
+    print('Lowest affected State is: ')
+    print('Lowest affected State count is: ')
+
+
+def Q1_6(json_file_path, start_date, end_date):
+    """Q1 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    print('Confirmed \n')
+    print('Day: ',Highest_spike_day)
+    print('Count: ',Highest_spike_count)
+    print('Recovered \n')
+    print('Day: ',Highest_spike_day)
+    print('Count: ',Highest_spike_count)
+    print('Deceased \n')
+    print('Day: ',Highest_spike_day)
+    print('Count: ',Highest_spike_count)
+
+
+def Q1_7(json_file_path, start_date, end_date):
+    """Q1 function : You have to count all the active cases and print the live active cases as on date.
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    print() # print any way you want
+
+
+def Q2_1(json_file_path, start_date, end_date):
+    """Q2 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    #plt.show()
+    #plt.save()
+
+def Q2_2(json_file_path, start_date, end_date):
+    """Q2 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    #plt.show()
+    #plt.save()
+
+
+def Q2_3(json_file_path, start_date, end_date):
+    """Q2 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    #plt.show()
+    #plt.save()
+
+def Q3(json_file_path, start_date, end_date):
+    """Q3 function
+    
+    Args:
+        json_file_path (TYPE): Description
+        start_date (TYPE): Description
+        end_date (TYPE): Description
+    """
+    return confirmed_intercept, confirmed_slope, recovered_intercept, recovered_slope, deceased_intercept, deceased_slope
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    print('Your Roll number') # Please put this first
+
+    start_date = "2020-03-14"
+    end_date = "2020-09-05"
+    
+    Q1_1('file_path.json', start_date, end_date):
+    #... Rest of the functions
